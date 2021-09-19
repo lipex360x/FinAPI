555fd143f9b5b1464c5ae0dd0d5cdacbc84fc60c
Felipe Borges <lipex360@gmail.com>
2021-09-18 23:10:23 -0300

    Get Balance

--- src/index.js
+++ src/index.js
@@ -143,4 +143,12 @@ app.delete("/account", (request, response) => {
   return response.status(200).json(customers)
 })
 
+app.get('/balance', (request, response) => {
+  const { statement } = request.customer
+
+  const balance = getBalance(statement)
+
+  return response.json(balance)
+})
+
 app.listen(3333)
