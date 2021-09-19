6e2483778c004b9befb3ccf0087d2758ff5afb52
Felipe Borges <lipex360@gmail.com>
2021-09-18 22:17:17 -0300

    Deposit statement

--- src/index.js
+++ src/index.js
@@ -54,4 +54,21 @@ app.get('/statement', (request, response) => {
   return response.json(customer.statement)
 })
 
+app.post('/deposit', (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request
+
+  const statmentOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statmentOperation)
+
+  return response.status(201).send()
+})
+
 app.listen(3333)
