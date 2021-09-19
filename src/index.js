886a211739a1716423d235da535515d57a413abd
Felipe Borges <lipex360@gmail.com>
2021-09-18 21:48:28 -0300

    Post account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require('express')
-
+const { v4: uuidv4 } = require('uuid')
+ 
 const app = express()
+app.use(express.json())
+
+const customers = [];
+
+/**
+ * cpf: string
+ * name: string
+ * id: uuid
+ * statement: []
+ */
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body
+  const id = uuidv4()
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: []
+  })
+
+  return response.status(201).send(customers)
+
+})
 
 app.listen(3333)
